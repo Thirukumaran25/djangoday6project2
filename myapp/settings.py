f532d895a7b86dac365ecc331fccaeb2e9503d57
Thirukumaran25 <kumaran2597@gmail.com>
2025-08-27 11:40:51 +0530

    Update settings.py

--- myapp/settings.py
+++ myapp/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-svjljsetup)n%h-n)oi=1+fiw+f51u*m#60u+s94&62_3$btyv'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
